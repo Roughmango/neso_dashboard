ca4d604e726fe49aea136ed9ea6b946898f1296d
timro <tr2g23@soton.ac.uk>
2026-08-15 20:39:53 +0100

    Updated data adding to do both national and regional and avoid duplicates

--- transform.py
+++ transform.py
@@ -7,42 +7,177 @@ from fetch import Fetch
 
 class Transform:
     def __init__(self):
-        self.API_URL = "https://api.carbonintensity.org.uk/intensity"
         self.DB_PATH = "carbon_intensity.db"
 
-    def transform(self):
+    def transformNational(self):
         fetcher = Fetch()
-        data, status_code = fetcher.fetch(self.API_URL)
-        if status_code==200:
-            fetched_at = datetime.now(timezone.utc).isoformat()
-            # Connect to SQLite database
-            connection = sqlite3.connect(self.DB_PATH)
+
+        data, status_code = fetcher.fetch(
+            "https://api.carbonintensity.org.uk/intensity"
+        )
+        fuel_data, fuel_status_code = fetcher.fetch(
+            "https://api.carbonintensity.org.uk/generation"
+        )
+        if status_code and fuel_status_code != 200:
+            print(f"National API request failed: {status_code, fuel_status_code}")
+            return
+
+        with sqlite3.connect(self.DB_PATH) as connection:
             cursor = connection.cursor()
+
             for reading in data:
+                # Create the time-period record if it doesn't already exist
+                self.insertPeriodID(cursor, reading)
+
+                # Get the ID for this time period
+                reading_id = self.getReadingID(cursor, reading)
+
+                # Insert national reading
                 cursor.execute(
                     """
-                    INSERT INTO national_readings (
-                        period_from,
-                        period_to,
+                    INSERT OR IGNORE INTO national_readings (
+                        reading_id,
                         forecast_intensity,
                         actual_intensity,
-                        intensity_index,
-                        fetched_at
+                        intensity_index
                     )
-                    VALUES (?, ?, ?, ?, ?, ?)
+                    VALUES (?, ?, ?, ?)
                     """,
                     (
-                        reading["from"],
-                        reading["to"],
-                        reading["intensity"]["forecast"],
-                        reading["intensity"]["actual"],
-                        reading["intensity"]["index"],
-                        fetched_at
+                        reading_id,
+                        reading["intensity"].get("forecast"),
+                        reading["intensity"].get("actual"),
+                        reading["intensity"].get("index")
                     )
                 )
+                self.transformNationalMix(cursor, reading_id, fuel_data["generationmix"])
 
-            connection.commit()
-            connection.close()
+
+    def transformRegional(self):
+        fetcher = Fetch()
+
+        data, status_code = fetcher.fetch(
+            "https://api.carbonintensity.org.uk/regional"
+        )
+
+        if status_code != 200:
+            print(f"Regional API request failed: {status_code}")
+            return
+
+        with sqlite3.connect(self.DB_PATH) as connection:
+            cursor = connection.cursor()
+
+            for period in data:
+
+                # Create the time-period record
+                self.insertPeriodID(cursor, period)
+
+                reading_id = self.getReadingID(cursor, period)
+
+                # Insert each region
+                for region in period["regions"]:
+
+                    # Insert regional reading
+                    cursor.execute(
+                        """
+                        INSERT OR IGNORE INTO regional_readings (
+                            reading_id,
+                            region_id,
+                            forecast_intensity,
+                            actual_intensity,
+                            intensity_index
+                        )
+                        VALUES (?, ?, ?, ?, ?)
+                        """,
+                        (
+                            reading_id,
+                            region["regionid"],
+                            region["intensity"].get("forecast"),
+                            region["intensity"].get("actual"),
+                            region["intensity"].get("index")
+                        )
+                    )
+                    self.transformRegionalMix(cursor, reading_id, region["generationmix"],region["regionid"])
+
+    def transformNationalMix(self,cursor, reading_id, fuel_data):
+        for fuel in fuel_data:
+            cursor.execute(
+                """
+                INSERT OR IGNORE INTO national_generation_mix (
+                    reading_id,
+                    fuel_type,
+                    percentage
+                )
+                VALUES (?, ?, ?)
+                """,
+                (
+                    reading_id,
+                    fuel["fuel"],
+                    fuel["perc"]
+                )
+            )
+
+    def transformRegionalMix(self, cursor, reading_id, fuel_data, region_id):
+        for fuel in fuel_data:
+            cursor.execute(
+                """
+                INSERT OR IGNORE INTO regional_generation_mix (
+                    reading_id,
+                    region_id,
+                    fuel_type,
+                    percentage
+                )
+                VALUES (?, ?, ?, ?)
+                """,
+                (
+                    reading_id,
+                    region_id,
+                    fuel["fuel"],
+                    fuel["perc"]
+                )
+            )
+
+    def insertPeriodID(self, cursor, reading):
+        now = datetime.now(timezone.utc)
+        minutes = (now.minute // 5) * 5
+
+        fetched_at = now.replace(
+            minute=minutes,
+            second=0,
+            microsecond=0
+        ).isoformat()
+        cursor.execute(
+            """
+            INSERT OR IGNORE INTO period_id (
+                period_from,
+                period_to,
+                fetched_at
+            )
+            VALUES (?, ?, ?)
+            """,
+            (
+                reading["from"],
+                reading["to"],
+                fetched_at
+            )
+        )
+
+    def getReadingID(self, cursor, reading):
+        cursor.execute(
+            """
+            SELECT id
+            FROM period_id
+            WHERE period_from = ?
+            AND period_to = ?
+            """,
+            (
+                reading["from"],
+                reading["to"]
+            )
+        )
+
+        return cursor.fetchone()[0]
 
 transform = Transform()
-transform.transform()
+transform.transformNational()
+transform.transformRegional()
