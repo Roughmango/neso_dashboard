fda888dbce92a7f0184e369347bce9960bce4b59
timro <tr2g23@soton.ac.uk>
2026-08-18 18:26:12 +0100

    added validation to pipeline

--- fetch.py
+++ fetch.py
@@ -1,5 +1,11 @@
 from transform import Transform
+from validate import Validate
 
+# performs the pipeline for fetching and validating data
+#transforms the data and adds it to the database
 transform = Transform()
 transform.transformNational()
 transform.transformRegional()
+#validates the data
+validate = Validate()
+validate.validate()
