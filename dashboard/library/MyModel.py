994eb5692e7a218e4d5d8f05e364a3ffc559761b
timro <tr2g23@soton.ac.uk>
2026-08-22 21:03:56 +0100

    Updated feature list to try and improve mae

--- dashboard/library/MyModel.py
+++ dashboard/library/MyModel.py
@@ -4,39 +4,46 @@ class MyModel:
     def model(self, data):
         data = data.copy()
         data = self.getFuelTable(data)
-        data["hour"] = data["period_from"].dt.hour
-        data["minute"] = data["period_from"].dt.minute
-        data["day_of_week"] = data["period_from"].dt.dayofweek
-        data["is_weekend"] = (data["day_of_week"] >= 5).astype(int)
-        data["time_of_day"] = (
-            data["hour"] * 2
-            + (data["minute"] // 30)
-        )
+        data = self.getFeatures(data)
+
+        # Target
         data["target"] = data["actual_intensity"]
+
+        # -------------------------
+        # Features
+        # -------------------------
 
         features = [
             "forecast_intensity",
+            "forecast_error",
+
             "hour",
             "minute",
             "day_of_week",
             "is_weekend",
             "time_of_day",
-            "biomass",
-            "coal",
-            "imports",
-            "gas",
-            "nuclear",
-            "other",
-            "hydro",
-            "solar",
-            "wind"
+
+            "lag_1",
+            "lag_2",
+            "lag_3",
+            "lag_4",
+            "lag_48",
+
+            "rolling_3",
+            "rolling_6",
+            "renewable",
+            "fossil",
+            "low_carbon"
         ]
 
-        # remove rows where there is not enough historical information
-
+        # Remove rows with missing values
         model_data = data.dropna(
             subset=features + ["target"]
         ).copy()
+
+        # -------------------------
+        # Train/test split
+        # -------------------------
 
         split = int(len(model_data) * 0.8)
 
@@ -49,19 +56,23 @@ class MyModel:
         X_test = test[features]
         y_test = test["target"]
 
+        # -------------------------
+        # Model
+        # -------------------------
 
         model = XGBRegressor(
-            n_estimators=300,
-            max_depth=6,
-            learning_rate=0.05,
+            n_estimators=500,
+            max_depth=5,
+            learning_rate=0.03,
+            subsample=0.8,
+            colsample_bytree=0.8,
             objective="reg:squarederror",
             random_state=42
         )
 
         model.fit(
             X_train,
-            y_train
-        )
+            y_train)
 
         predictions = model.predict(X_test)
         results = pd.DataFrame({
@@ -70,7 +81,7 @@ class MyModel:
         })
 
 
-        return results.head()
+        return results
 
 
     def getFuelTable(self, data):
@@ -96,3 +107,63 @@ class MyModel:
             how="left"
         )
         return finalData
+
+    def getFeatures(self, data):
+        data["hour"] = data["period_from"].dt.hour
+        data["minute"] = data["period_from"].dt.minute
+        data["day_of_week"] = data["period_from"].dt.dayofweek
+        data["is_weekend"] = (data["day_of_week"] >= 5).astype(int)
+        data["time_of_day"] = (
+                data["hour"] * 2
+                + (data["minute"] // 30)
+        )
+        data["target"] = data["actual_intensity"]
+
+        data["lag_1"] = data["actual_intensity"].shift(1)
+        data["lag_2"] = data["actual_intensity"].shift(2)
+        data["lag_3"] = data["actual_intensity"].shift(3)
+        data["lag_4"] = data["actual_intensity"].shift(4)
+
+        # Same time yesterday
+        data["lag_48"] = data["actual_intensity"].shift(48)
+
+        # Rolling averages
+        data["rolling_3"] = (
+            data["actual_intensity"]
+            .shift(1)
+            .rolling(3)
+            .mean()
+        )
+
+        data["rolling_6"] = (
+            data["actual_intensity"]
+            .shift(1)
+            .rolling(6)
+            .mean()
+        )
+
+        data["renewable"] = (
+                data["biomass"]
+                + data["hydro"]
+                + data["solar"]
+                + data["wind"]
+        )
+
+        data["fossil"] = (
+                data["coal"]
+                + data["gas"]
+        )
+
+        data["low_carbon"] = (
+                data["nuclear"]
+                + data["hydro"]
+                + data["solar"]
+                + data["wind"]
+                + data["biomass"]
+        )
+
+        data["forecast_error"] = (
+                data["actual_intensity"]
+                - data["forecast_intensity"]
+        )
+        return data
